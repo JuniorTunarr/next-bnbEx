d6ddc026c3fffb7f430b97db501ab3dad4473984
JuniorTunarr <shclgus111@gmail.com>
2023-03-21 22:28:23 +0900

    0321

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -471,15 +471,12 @@ const SignUpModal: ForwardRefRenderFunction<HTMLInputElement, IProps> = (
             password
           );
           alert("회원 가입이 완료되었습니다.");
+          const { data } = await signupAPI(signUpBody);
+          dispatch(userActions.setLoggedUser(data));
         } else {
           userData = await signInWithEmailAndPassword(fbAuth, email, password);
           alert("이미 가입된 계정이 있습니다.");
         }
-        // const userResponse = await axios.post("/api/auth/signup", signUpBody);
-        // const user = userResponse.data;
-        // dispatch(userActions.setLoggedUser(user));
-        const { data } = await signupAPI(signUpBody);
-        dispatch(userActions.setLoggedUser(data));
         closeModal();
       } catch (e) {
         console.log(e);
